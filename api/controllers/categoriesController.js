dbd19c6fb196121913eaa23f87ffbd3ff92ee1c8
germauricio <mpauluk@est.frba.utn.edu.ar>
2021-02-15 21:42:40 -0300

    overflow hidden

--- api/controllers/categoriesController.js
+++ api/controllers/categoriesController.js
@@ -1,28 +1,28 @@
 const axios = require('axios');
 
 exports.show = async (req, res) => {
-    const id = req.params.id;
-    try {
-      const response = await axios.get(
-          `${process.env.API_MELI}/categories/`+ id,
-      );
-      const categories = {
-          parentCategories: response.data.path_from_root
-      }
-      res.json(categories);
-    } catch (error) {
-      res.json(error.message);
-    }
-  };
-
-  exports.getItemCategory = async (req, res) => {
-    const id = req.params.id;
-    try {
-      const response = await axios.get(
-          `${process.env.API_MELI}/items/`+ id,
-      );
-      res.json(response.data.category_id);
-    } catch (error) {
-      res.json(error.message);
-    }
+  const id = req.params.id;
+  try {
+    const response = await axios.get(
+        `${process.env.API_MELI}/categories/`+ id,
+    );
+    const categories = {
+      parentCategories: response.data.path_from_root,
+    };
+    res.json(categories);
+  } catch (error) {
+    res.json(error.message);
   }
+};
+
+exports.getItemCategory = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const response = await axios.get(
+        `${process.env.API_MELI}/items/`+ id,
+    );
+    res.json(response.data.category_id);
+  } catch (error) {
+    res.json(error.message);
+  }
+};
--- services/categoryService.js
+++ services/categoryService.js
@@ -1,54 +1,53 @@
 import axios from '../lib/axios.js';
 
 export const getCategories = (categories) =>
-    new Promise((resolve, reject) => {
-        const category = mode(categories);
-        axios.get(`/api/categories/${category}`)
+  new Promise((resolve, reject) => {
+    const category = mode(categories);
+    axios.get(`/api/categories/${category}`)
         .then((res) => {
-            resolve(res.data);
+          resolve(res.data);
         })
         .catch((err) => {
-            if (err.response && err.response.data && err.response.data.message) {
-                reject(new Error(err.response.data.message));
-            } else {
-                reject(new Error('Error inesperado'));
-            }
+          if (err.response && err.response.data && err.response.data.message) {
+            reject(new Error(err.response.data.message));
+          } else {
+            reject(new Error('Error inesperado'));
+          }
         });
-    });
+  });
 
 export const getItemCategory = (itemId) =>
-    new Promise((resolve, reject) => {
-        axios.get(`/api/categories/item/${itemId}`)
+  new Promise((resolve, reject) => {
+    axios.get(`/api/categories/item/${itemId}`)
         .then((res) => {
-            resolve(res.data);
+          resolve(res.data);
         })
         .catch((err) => {
-            if (err.response && err.response.data && err.response.data.message) {
-                reject(new Error(err.response.data.message));
-            } else {
-                reject(new Error('Error inesperado'));
-            }
+          if (err.response && err.response.data && err.response.data.message) {
+            reject(new Error(err.response.data.message));
+          } else {
+            reject(new Error('Error inesperado'));
+          }
         });
-    });
+  });
 
-function mode(array)
-{
-    if(array.length == 0)
-        return null;
-    var modeMap = {};
-    var maxEl = array[0], maxCount = 1;
-    for(var i = 0; i < array.length; i++)
-    {
-        var el = array[i];
-        if(modeMap[el] == null)
-            modeMap[el] = 1;
-        else
-            modeMap[el]++;  
-        if(modeMap[el] > maxCount)
-        {
-            maxEl = el;
-            maxCount = modeMap[el];
-        }
+function mode(array) {
+  if (array.length == 0) {
+    return null;
+  }
+  const modeMap = {};
+  let maxEl = array[0]; let maxCount = 1;
+  for (let i = 0; i < array.length; i++) {
+    const el = array[i];
+    if (modeMap[el] == null) {
+      modeMap[el] = 1;
+    } else {
+      modeMap[el]++;
     }
-    return maxEl;
+    if (modeMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = modeMap[el];
+    }
+  }
+  return maxEl;
 }
